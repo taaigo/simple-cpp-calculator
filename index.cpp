b29cb29107242e4f0261b578793ea623dce373e1
taigochan <69469791+taigochan@users.noreply.github.com>
2022-03-19 21:47:50 +0100

    added decimals
    Added decimal support by changing variable types to float. And slight improvement by adding switch statement instead of else if. And changed the "opr" variable data type to char. (lmao ikr epic update)

--- index.cpp
+++ index.cpp
@@ -1,62 +1,69 @@
 #include <iostream>
+
 using namespace std;
 
-string opr;
+char opr;
 
-int main() {
-cout << "What operator will you use?\nYou can choose between +, -, x and :" << endl << "->";
-cin >> opr;
+int main()
+{
+    cout << "What operator will you use?\nYou can choose between +, -, x and :" << endl << "->";
+    cin >> opr;
 
-int num1;
-int num2;
+    float num1, num2;
 
-if (opr == "+") {
-		cout << "Insert a number.\n<...> + ..." << endl;
-		cin >> num1;
-		cout << "Insert another number." << endl << num1 << " + <...>" << endl;
-		cin >> num2;
+    switch (opr)
+    {
+        case '+':
+        {
+            cout << "Insert a number.\n<...> + ..." << endl;
+            cin >> num1;
+            cout << "Insert another number." << endl << num1 << " + <...>" << endl;
+            cin >> num2;
 
-		int answerAdd = num1 + num2;
+            float answerAdd = num1 + num2;
 
-		cout << "Here's your answer.\n" << num1 << " + " << num2 << " = " << answerAdd << endl;
-		return 0;
+            cout << "Here's your answer.\n" << num1 << " + " << num2 << " = " << answerAdd << endl;
+            break;
+        }
+        case '-':
+        {
+            cout << "Insert a number.\n<...> - ..." << endl;
+            cin >> num1;
+            cout << "Insert another number." << endl << num1 << " - <...>" << endl;
+            cin >> num2;
 
-	} else if (opr == "-") {
-		cout << "Insert a number.\n<...> - ..." << endl;
-		cin >> num1;
-		cout << "Insert another number." << endl << num1 << " - <...>" << endl;
-		cin >> num2;
+            float answerSub = num1 - num2;
 
-		int answerSub = num1 - num2;
+            cout << "Here's your answer.\n" << num1 << " - " << num2 << " = " << answerSub << endl;
+            break;
+        }
+        case 'x':
+        {
+            cout << "Insert a number.\n<...> x ..." << endl;
+            cin >> num1;
+            cout << "Insert another number." << endl << num1 << " x <...>" << endl;
+            cin >> num2;
 
-		cout << "Here's your answer.\n" << num1 << " - " << num2 << " = " << answerSub << endl;
-		return 0;
+            float answerMul = num1 * num2;
 
-	} else if (opr == "x") {
-		cout << "Insert a number.\n<...> x ..." << endl;
-		cin >> num1;
-		cout << "Insert another number." << endl << num1 << " x <...>" << endl;
-	        cin >> num2;
+            cout << "Here's your answer." << endl << num1 << " x " << num2 << " = " << answerMul << endl;
+            break;
+        }
+        case ':' || '/':
+        {
+            cout << "Insert a number.\n<...> : ..." << endl;
+            cin >> num1;
+            cout << "Insert another number." << endl << num1 << " : <...>" << endl;
+            cin >> num2;
 
-		int answerMul = num1 * num2;
+            float answerDev = num1 / num2;
 
-		cout << "Here's your answer." << endl << num1 << " x " << num2 << " = " << answerMul << endl;
-		return 0;	
-	
-	} else if (opr == ":") {
-		cout << "Insert a number.\n<...> : ..." << endl;
-		cin >> num1;
-		cout << "Insert another number." << endl << num1 << " : <...>" << endl;
-		cin >> num2;
-		
-		int answerDev = num1 / num2;
-
-		cout << "Here's your answer." << endl << num1 << " : " << num2 << " = " << answerDev << endl;
-		return 0;
-	
-	} else if (opr != "+" || "-" || "x" || ":") {
-		cout << "That is not an supported operator" << endl;
-	return 0;
-
-	}
+            cout << "Here's your answer." << endl << num1 << " : " << num2 << " = " << answerDev << endl;
+            break;
+        }
+        default:
+        {
+            cout << "That is not an supported operator" << endl;
+        }
+    }
 }
